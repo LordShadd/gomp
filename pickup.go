9e2ab51f173282bc9e2f01ae29e7ee4d0a952bcc
LordShadd <real.davidwilson@proton.me>
2026-02-12 02:49:02 -0300

    fix(pickup): protect api with mutex

--- pickup.go
+++ pickup.go
@@ -46,6 +46,9 @@ func PickupFromID(pickupID int) *Pickup {
 
 func PickupCreate(model, pickupType int, x, y, z float32,
 	virtualWorld int) *Pickup {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	var pickupID C.int
 
 	ptr := C.Pickup_Create(C.int(model), C.int(pickupType), C.float(x), C.float(y), C.float(z), C.int(virtualWorld), &pickupID)
@@ -61,22 +64,37 @@ func PickupCreate(model, pickupType int, x, y, z float32,
 
 func PickupAddStatic(model int, pickupType int, x, y, z float32,
 	virtualWorld int) bool {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return bool(C.Pickup_AddStatic(C.int(model), C.int(pickupType), C.float(x), C.float(y), C.float(z), C.int(virtualWorld)))
 }
 
 func (p *Pickup) Destroy() bool {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return bool(C.Pickup_Destroy(p.ptr))
 }
 
 func (p *Pickup) GetID() int {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return int(C.Pickup_GetID(p.ptr))
 }
 
 func (p *Pickup) IsValid() bool {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return bool(C.Pickup_IsValid(p.ptr))
 }
 
 func (p *Pickup) IsStreamedIn(player *Player) bool {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	if p == nil || p.ptr == nil {
 		return false
 	}
@@ -85,6 +103,9 @@ func (p *Pickup) IsStreamedIn(player *Player) bool {
 }
 
 func (p *Pickup) GetPos() (float32, float32, float32, bool) {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	var x, y, z C.float
 
 	ret := C.Pickup_GetPos(p.ptr, &x, &y, &z)
@@ -93,41 +114,71 @@ func (p *Pickup) GetPos() (float32, float32, float32, bool) {
 }
 
 func (p *Pickup) GetModel() int {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return int(C.Pickup_GetModel(p.ptr))
 }
 
 func (p *Pickup) GetType() int {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return int(C.Pickup_GetType(p.ptr))
 }
 
 func (p *Pickup) GetVirtualWorld() int {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return int(C.Pickup_GetVirtualWorld(p.ptr))
 }
 
 func (p *Pickup) SetPos(x, y, z float32, update bool) bool {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return bool(C.Pickup_SetPos(p.ptr, C.float(x), C.float(y), C.float(z), C.bool(update)))
 }
 
 func (p *Pickup) SetModel(model int, update bool) bool {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return bool(C.Pickup_SetModel(p.ptr, C.int(model), C.bool(update)))
 }
 
 func (p *Pickup) SetType(pickupType int, update bool) bool {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return bool(C.Pickup_SetType(p.ptr, C.int(pickupType), C.bool(update)))
 }
 
 func (p *Pickup) SetVirtualWorld(virtualworld int) bool {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return bool(C.Pickup_SetVirtualWorld(p.ptr, C.int(virtualworld)))
 }
 
 func (p *Pickup) ShowForPlayer(player *Player) bool {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return bool(C.Pickup_ShowForPlayer(player.ptr, p.ptr))
 }
 
 func (p *Pickup) HideForPlayer(player *Player) bool {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return bool(C.Pickup_HideForPlayer(player.ptr, p.ptr))
 }
 
 func (p *Pickup) IsHiddenForPlayer(player *Player) bool {
+	apiMutex.Lock()
+	defer apiMutex.Unlock()
+
 	return bool(C.Pickup_IsHiddenForPlayer(player.ptr, p.ptr))
 }
